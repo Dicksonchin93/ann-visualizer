7b552426053e475a8c5657ba64bd619338af8e66
Dicksonchin93 <chin.ee.kin@datarobot.com>
2019-08-19 16:52:39 +0800

    change travis test

--- setup.py
+++ setup.py
@@ -1,57 +1,50 @@
 from setuptools import setup
 
-long_description = """
-    ANN Visualizer is a great visualization python library used to work with Keras. It uses python's graphviz library to create a presentable graph of the neural network you are building.
-
-    Usage:
+with open("README.md") as readme_file:
+    readme = readme_file.read()
 
 
-from keras_architecture_visualizer.visualize import ann_viz;
-#Build your model here
-ann_viz(model)
+def read_requirements(fname):
+    with open(fname) as requirements_file:
+        requirements = requirements_file.read().split("\n")
+        return [x for x in requirements if x.strip() != ""]
 
+requirements = read_requirements("requirements.txt")
+test_requirements = requirements
 
-
-Documentation:
-
-ann_viz(model, view=True, filename="network.gv")
-
- model - The Keras Sequential model
- view - If True, it opens the graph preview after executed
- filename - Where to save the graph. (.gv file format)
-""";
+setup_requirements = ["pytest-runner"]
 
 
 setup(
-  name = 'keras_architecture_visualizer',
-  packages = ['keras_architecture_visualizer'],
-  version = '2.5',
-  license="MIT",
-  description = 'A python library for visualizing Neural Networks',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  author = 'Tudor Gheorghiu',
-  author_email = 'tudor.posta@live.com',
-  url = 'https://github.com/Prodicode/ann-visualizer',
-  download_url = '',
-  keywords = ['ann', 'ai', 'visualizer', 'learning', 'artificial', 'intelligence'],
-  classifiers = [
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 4 - Beta',
+    name='keras_architecture_visualizer',
+    packages=['keras_architecture_visualizer'],
+    version=__version__,
+    license="MIT",
+    description='A Python Library for visualizing Keras Neural Networks Models',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Chin Ee Kin',
+    author_email='dickson_chin93@hotmail.com',
+    url='https://github.com/Dicksonchin93/keras-architecture-visualizer',
+    download_url='',
+    keywords=['ann', 'ai', 'visualizer', 'learning', 'artificial', 'intelligence'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
 
-    # Indicate who your project is intended for
-    'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'Topic :: Scientific/Engineering :: Visualization',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Visualization',
 
-    # Pick your license as you wish (should match "license" above)
-     'License :: OSI Approved :: MIT License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 3.6',
-],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
 )
